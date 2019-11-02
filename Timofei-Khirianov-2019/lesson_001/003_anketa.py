96a06ce7830bbe098edb3cf5fca726162afd785c
Alexander <anklav24@gmail.com>
2019-11-03 04:06:55 +0700

    Add 003_anketa.py Find age

--- Timofei-Khirianov-2019/lesson_001/003_anketa.py
+++ Timofei-Khirianov-2019/lesson_001/003_anketa.py
@@ -1,3 +1,22 @@
-print('What is your name?')
-name = input()
-print('Nice to meet you!', name + '.')
+name = input('Hello! What is your name? : ')
+print('Nice to meet you,', name + '!')
+print()
+
+age = int(input('How old are you ' + name + '? : '))
+print()
+
+# Little test
+# for i in range(200):
+#     x = age + i
+x = age + 1
+
+print('А я думал тебе', x, end=' ')
+if x >= 11 and x <= 19:
+    print('лет', end='')
+elif x % 10 == 1:
+    print('год', end='')
+elif x % 10 >= 2 and x % 10 <= 4:
+    print('года', end='')
+else:
+    print('лет', end='')
+print('!')
