a630fab118cee7530e564f4a9c299e6f59baa7b1
Alexander Lobanov <anklav24@gmail.com>
2019-11-05 00:54:37 +0700

    Add 12_3_using_tuple.py

--- A-Byte-of-Python/12_3_using_tuple.py
+++ A-Byte-of-Python/12_3_using_tuple.py
@@ -3,7 +3,7 @@ print('The numbest of animals in a zoo -', len(zoo))
 
 new_zoo = 'Monkey', 'Camel', zoo
 print('Количество клеток в зоопарке -', len(new_zoo))
-print('All animals in new zoo -', new_zoo)
+print('All animals in a new zoo -', new_zoo)
 print('Животные привезенные из старого зоопарка -', new_zoo[2])
 print('Последнее животное, привезенное из старого зоопарка -', new_zoo[2][2])
 print('Количество животных в новом зоопарке -', len(new_zoo)-1 + len(new_zoo[2]))
