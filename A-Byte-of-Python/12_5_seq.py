4a201bfe2588a51e7ea0edb16bbfd2aa181c3660
Alexander Lobanov <anklav24@gmail.com>
2019-11-05 22:07:44 +0700

    Add 12_5_seq.py

---
+++ A-Byte-of-Python/12_5_seq.py
@@ -0,0 +1,26 @@
+shoplist = ['Apples', 'Mangoes', 'Carrots', 'Bananas']
+name = 'swaroop'
+
+# Операция индексирования
+print('Element 0 -', shoplist[0])
+print('Element 1 -', shoplist[1])
+print('Element 2 -', shoplist[2])
+print('Element 3 -', shoplist[3])
+print('Element -1 -', shoplist[-1])
+print('Element -2 -', shoplist[-2])
+print('Element 0 -', shoplist[0])
+print()
+
+# Вырезка из списка
+print('Elements from 1 to 3:', shoplist[1:3])
+print('Elements from 2 to end:', shoplist[2:])
+print('Elements from 1 to -1:', shoplist[1:-1])
+print('Elements from start to end:', shoplist[:])
+print()
+
+# Вырезка из строки
+print('Symbols from 1 to 3:', name[1:3])
+print('Symbols from 2 to end:', name[2:])
+print('Symbols from 1 to -1:', name[1:-1])
+print('Symbols from start to end:', name[:])
+print()
