ab7a83c499c50f9deab4106dfa81c21e79c9bf57
Alexander Lobanov <anklav24@gmail.com>
2020-02-09 07:18:10 +0700

    Solve 2_6_11

---
+++ stepik/programming on python/2_6_11_my_solve.py
@@ -0,0 +1,41 @@
+"""
+Дополнительная
+Выведите таблицу размером n×n, заполненную числами от 1 до n^2
+  по спирали, выходящей из левого верхнего угла и закрученной по часовой стрелке, как показано в примере (здесь n=5n=5):
+
+Sample Input:
+5
+Sample Output:
+1 2 3 4 5
+16 17 18 19 6
+15 24 25 20 7
+14 23 22 21 8
+13 12 11 10 9
+
+Тестируется через stdin → stdout
+"""
+n = 5
+# n = int(input())
+m = [[0 for j in range(n)] for i in range(n)]
+count = 1
+a = 0
+
+for i in range(n**2):
+    for j in range(a, n - a):
+        m[i][j] = count
+        count += 1
+    for i in range(a + 1, n - a):
+        m[i][j] = count
+        count += 1
+    for j in range(-(2 + a), -(n + 1 - a), -1):
+        m[i][j] = count
+        count += 1
+    for i in range(-(2 + a), -(n - a), -1):
+        m[i][j] = count
+        count += 1
+    a += 1
+
+for i in range(n):
+    print()
+    for j in range(n):
+        print(m[i][j], end=' ')
