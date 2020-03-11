29066cd0e7c0e4aa76181248a0d8d971c4efaaa7
Alexander Lobanov <anklav24@gmail.com>
2020-03-12 01:49:12 +0700

    Solve the 3_4_4 task

---
+++ stepik/programming_on_python/3_4_4_my_solve.py
@@ -0,0 +1,25 @@
+import os
+
+dataset = os.path.join('C:\\', 'Users', 'Admin', 'Downloads', 'dataset_3363_4 ' + '(7)' + '.txt')
+print(dataset)
+answer = os.path.join('C:\\', 'Users', 'Admin', 'Downloads', 'answer.txt')
+print(answer)
+first_sum, second_sum, third_sum, count = 0, 0, 0, 0
+
+with open(dataset) as inf, open(answer, 'w') as ouf:
+    for string in inf:
+        student = string.strip().split(sep=';')
+        average = (int(student[1]) + int(student[2]) + int(student[3]))/3
+        ouf.write(str(round(average, 9)) + '\n')
+        first_sum += int(student[1])
+        second_sum += int(student[2])
+        third_sum += int(student[3])
+        count += 1
+
+with open(answer, 'a') as ouf:
+    ouf.write(str(round(first_sum / count, 9)) + ' ' + str(round(second_sum / count, 9)) + ' ' + str(round(third_sum / count, 9)))
+
+with open(answer) as inf:
+    for string in inf:
+        print(string.strip())
+print('Count:', count)
