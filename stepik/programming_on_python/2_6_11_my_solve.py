591b09facd8ea55dd495de6df4bb29e60c7c6550
Alexander Lobanov <anklav24@gmail.com>
2020-06-18 15:31:02 +0700

    5_6_3_tasks_nested_lists.ipynb

--- stepik/programming_on_python/2_6_11_my_solve.py
+++ stepik/programming_on_python/2_6_11_my_solve.py
@@ -20,7 +20,7 @@ m = [[0 for j in range(n)] for i in range(n)]
 count = 1
 a = 0
 
-for i in range(int(n/2 + 1)):
+for i in range(int(n / 2 + 1)):
     for j in range(a, n - a):
         # m[i][j] = count
         m[i][j] = "→"
