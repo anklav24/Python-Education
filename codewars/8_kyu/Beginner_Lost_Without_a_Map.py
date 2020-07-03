17c49858c4089ce2c81113ec375404b8b82d867c
Alexander Lobanov <anklav24@gmail.com>
2020-07-03 15:44:54 +0700

    Solve some tasks from Codewars

---
+++ codewars/8_kyu/Beginner_Lost_Without_a_Map.py
@@ -0,0 +1,15 @@
+def maps(a):
+    return [i * 2 for i in a]
+
+print(maps([1, 2, 3]))
+
+def maps_comm_1(a):
+    return [x * 2 for x in a]
+
+print(maps_comm_1([1, 2, 3]))
+
+def maps_comm_2(a):
+    return map(lambda x: x * 2, a)
+
+print(maps_comm_2([1, 2, 3]))
+
