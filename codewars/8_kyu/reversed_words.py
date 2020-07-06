9772f5f4a476ace8491b3e8e128c3999db1420ee
Alexander Lobanov <anklav24@gmail.com>
2020-07-06 16:08:50 +0700

    Solve some tasks from Codewars

---
+++ codewars/8_kyu/reversed_words.py
@@ -0,0 +1,13 @@
+def reverseWords(str):
+    """Reverses all of the words within the string passed in."""
+    lst = str.split()
+    lst.reverse()
+    return ' '.join(lst)
+
+def reverseWords(str: str) -> str:
+    """Reverses all of the words within the string passed in."""
+    return ' '.join(str.split()[::-1])
+
+
+print(reverseWords("The greatest victory is that which requires no battle"))
+print(reverseWords("hello world!"))
