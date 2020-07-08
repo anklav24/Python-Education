647323ac5660c6e81a3e77dc32c9d80d03d99e0e
Alexander Lobanov <anklav24@gmail.com>
2020-07-08 21:29:09 +0700

    6_2_2_tasks_dict.ipynb

---
+++ chekio/sum_numbers.py
@@ -0,0 +1,19 @@
+def sum_numbers(text: str) -> int:
+    """In a given text sum the numbers."""
+    text = text.split()
+    summ = []
+    for i in text:
+        if i.isdigit():
+            summ.append(int(i))
+    return sum(summ)
+
+
+def sum_numbers(text: str) -> int:
+    """In a given text sum the numbers."""
+    return sum(int(word) for word in text.split() if word.isdigit())
+
+sum_numbers('hi')  #0
+sum_numbers('who is 1st here') #0
+sum_numbers('my numbers is 2') #2
+sum_numbers('5 plus 6 is') #11
+sum_numbers('') #0
