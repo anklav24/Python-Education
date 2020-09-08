5c796df1b10ab0550f35dbd1b2c28eea4c6bd1c8
Aleksandr Lobanov <anklav24@gmail.com>
2020-09-09 01:01:32 +0700

    Add tests to codewars\8_kyu\stringy_strings.py

--- codewars/8_kyu/stringy_strings.py
+++ codewars/8_kyu/stringy_strings.py
@@ -6,27 +6,28 @@ def stringy(size):
         return str("10" * int(size / 2 + 1))[:-1]
 
 
-assert stringy(2) == "10", f'{stringy(2)} should equal "10"'
-assert stringy(3) == "101", f'{stringy(3)} should equal "101"'
-assert stringy(4) == "1010", f'{stringy(4)} should equal "1010"'
-assert stringy(5) == "10101", f'{stringy(5)} should equal "10101"'
-
-assert stringy(10)[0] == '1', f'"Whoops your string doesnt start with a 1". You received {stringy(10)}'
-
-for i in range(1, 5):
-    assert len(stringy(i)) == i, f"Make sure your string is the right length: {len(stringy(i))} should equal {i}."
-
-
 def stringy_community(size):
     return "10" * (size // 2) + "1" * (size % 2)
 
 
-assert stringy_community(2) == "10", f'{stringy_community(2)} should equal "10"'
-assert stringy_community(3) == "101", f'{stringy_community(3)} should equal "101"'
-assert stringy_community(4) == "1010", f'{stringy_community(4)} should equal "1010"'
-assert stringy_community(5) == "10101", f'{stringy_community(5)} should equal "10101"'
+def test_functions_stringy(name_of_test_func):
+    assert name_of_test_func(2) == "10", f'{name_of_test_func}: {name_of_test_func(2)} should equal "10"'
+    assert name_of_test_func(3) == "101", f'{name_of_test_func}: {name_of_test_func(3)} should equal "101"'
+    assert name_of_test_func(4) == "1010", f'{name_of_test_func}: {name_of_test_func(4)} should equal "1010"'
+    assert name_of_test_func(5) == "10101", f'{name_of_test_func}: {name_of_test_func(5)} should equal "10101"'
 
-assert stringy_community(10)[0] == '1', f'"Whoops your string doesnt start with a 1". You received {stringy_community(10)}'
+    assert name_of_test_func(10)[0] == '1', f'"Whoops your string doesnt start with a 1". You received {name_of_test_func(10)}'
 
-for i in range(1, 5):
-    assert len(stringy_community(i)) == i, f"Make sure your string is the right length: {len(stringy_community(i))} should equal {i}."
+    for i in range(1, 5):
+        assert len(name_of_test_func(i)) == i, f"Make sure your string is the right length: {len(name_of_test_func(i))} should equal {i}."
+
+
+# Let's see our results.
+for i in range(6):
+    print(f'{stringy(i)=}, {i=}')
+
+for i in range(6):
+    print(f'{stringy_community(i)=}, {i=}')
+
+test_functions_stringy(stringy)
+test_functions_stringy(stringy_community)
